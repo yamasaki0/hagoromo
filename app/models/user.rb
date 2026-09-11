67ad0291f576dd374326964b635234ad979fe1c9
yamasaki <yamasaki@comcre.co.jp>
2026-09-11 18:05:36 +0900

    deviseいれた

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
   # user.rb
   has_many :posts
   has_many :reactions
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get "ranking/index"
   root "home#index"
 
